03247500db93bda4a46f1d5277e380fe068b3151
siam02 <siam.jaman02@gmail.com>
2025-08-14 23:30:46 +0600

    Add program to calculate the sum of two integers

---
+++ Add_Two_Numbers.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int a, b;
+
+    cout << "Enter two integers: ";
+    cin >> a >> b;
+
+    cout << "Sub of the integers: " << a+b << endl;
+
+    return 0;
+}
