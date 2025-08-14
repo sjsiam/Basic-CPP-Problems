30451de1893a8cb67a1ad0c5e8021555c4e26d91
siam02 <siam.jaman02@gmail.com>
2025-08-15 00:14:56 +0600

    Refactor prime number program for improved readability and user prompts

--- Prime_Numbers_1_to_N.cpp
+++ Prime_Numbers_1_to_N.cpp
@@ -1,29 +1,29 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-int main() {
+int main()
+{
     int n;
 
-    cout << "Enter a number: ";
     cin >> n;
 
-    cout << "Prime numbers from 1 to " << n << " are: ";
-    
-    for (int i = 2; i <= n; i++) {
+    for (int i = 2; i <= n; i++)
+    {
         bool isPrime = true;
 
-        for (int j = 2; j <= i/2; j++) {
-            if (i % j == 0) {
+        for (int j = 2; j <= i / 2; j++)
+        {
+            if (i % j == 0)
+            {
                 isPrime = false;
                 break;
             }
         }
 
-        if (isPrime) {
+        if (isPrime)
+        {
             cout << i << " ";
         }
     }
-
-    cout << endl;
     return 0;
 }
