f862a939f1f2368f404ba9ad347793ee19d75f81
siam02 <siam.jaman02@gmail.com>
2025-08-15 00:50:59 +0600

    Add program to find the largest of three integers

---
+++ Largest_Number.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a, b, c;
+    cout << "Enter three integers: ";
+    cin >> a >> b >> c;
+    if (a >= b && a >= c) {
+        cout << "Largest number is: " << a << endl;
+    } else if (b >= a && b >= c) {
+        cout << "Largest number is: " << b << endl;
+    } else {
+        cout << "Largest number is: " << c << endl;
+    }
+}
