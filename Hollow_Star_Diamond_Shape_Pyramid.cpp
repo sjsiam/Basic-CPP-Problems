9dc76c809c0168284d4df93d5eaf77b13518f3b2
siam02 <siam.jaman02@gmail.com>
2025-08-17 22:56:09 +0600

    Program: Hollow Star Diamond

---
+++ Hollow_Star_Diamond_Shape_Pyramid.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Enter number of rows for half diamond: ";
+    cin >> n;
+
+    cout << endl;
+
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= n - i; j++) {
+            cout << " ";
+        }
+
+        for (int k = 1; k <= (2 * i - 1); k++) {
+            if (k == 1 || k == (2 * i - 1)) {
+                cout << "*";
+            } else {
+                cout << " ";
+            }
+        }
+
+        cout << endl;
+    }
+
+    for (int i = n - 1; i >= 1; i--) {
+        for (int j = 1; j <= n - i; j++) {
+            cout << " ";
+        }
+
+        for (int k = 1; k <= (2 * i - 1); k++) {
+            if (k == 1 || k == (2 * i - 1)) {
+                cout << "*";
+            } else {
+                cout << " ";
+            }
+        }
+
+        cout << endl;
+    }
+
+    return 0;
+}
