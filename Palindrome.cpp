b79e6c1f95adfd8b1841e3d1c2e6ad308f9b3c22
siam02 <siam.jaman02@gmail.com>
2025-08-17 21:35:52 +0600

    1.	CPP Program to Check Whether a Number is a Palindrome or Not

---
+++ Palindrome.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n;
+
+    cin >> n;
+
+    int original = n;
+    int rev = 0;
+
+    while (n > 0)
+    {
+        int rem = n % 10;
+
+        rev = rev * 10 + rem;
+
+        n = n / 10;
+    }
+
+    if (original == rev)
+    {
+        cout << "Palindrome" << endl;
+    }
+    else
+    {
+        cout << "Not Palindrome" << endl;
+    }
+
+    return 0;
+}
